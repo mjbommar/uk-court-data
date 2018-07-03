fab9df53d712daed08c3fa68caaa6bf354b96d2c
mjbommar <michael@bommaritollc.com>
2018-07-03 18:07:03 -0400

    handle alternative judgment format

--- src/extract_case_text.py
+++ src/extract_case_text.py
@@ -1,24 +1,31 @@
 # Imports
 import multiprocessing
 import os
-import pandas
 import sys
+
+# Packages
+import pandas
 import tika.parser
 
+# Project imports
 from download_case_data import CASE_OUTPUT_PATH
+
+# Path constants
 CASE_TEXT_PATH = "data/text/"
 
-def extract_case_judgement_text(case_file_path):
+
+def extract_case_judgement_text(case_file_path, skip_existing=True):
     """
     Extract text from case file.
     :param case_file_path:
+    :param skip_existing:
     :return:
     """
     # Download path
     case_file_name = case_file_path
     text_file_name = case_file_path.replace("data/cases/", "data/text/")
 
-    if os.path.exists(text_file_name):
+    if os.path.exists(text_file_name) and skip_existing:
         return True
     elif not os.path.exists(case_file_name):
         return False
@@ -27,15 +34,16 @@ def extract_case_judgement_text(case_file_path):
             os.makedirs(os.path.dirname(text_file_name))
         except FileExistsError as e:
             pass
-                
+
     # get case text
-    case_text = None
     case_file_buffer = open(case_file_path, "rb").read()
     tika_response = tika.parser.from_buffer(case_file_buffer)
     if "content" in tika_response:
         if len(tika_response["content"]) > 0:
             content = tika_response["content"]
             p0 = content.find("HTML VERSION OF JUDGMENT")
+            if p0 == -1:
+                p0 = content.find("Crown Copyright")
             p1 = content.find("BAILII:\nCopyright Policy")
             case_text = content[p0:p1].strip()
         else:
@@ -45,10 +53,10 @@ def extract_case_judgement_text(case_file_path):
 
     # output to file
     with open(text_file_name, "w") as output_file:
-        output_file.write(case_text)    
+        output_file.write(case_text)
 
 
-def extract_all_cases(decision_file_path, output_path=None, skip_existing=True, workers=1):
+def extract_all_cases(decision_file_path, skip_existing=True, workers=1):
     """
     Download all cases listed in the given decision file.
     :param decision_file_path:
@@ -64,17 +72,16 @@ def extract_all_cases(decision_file_path, output_path=None, skip_existing=True, 
     if workers == 1:
         for row_id, row in decision_df.iterrows():
             case_file_name = os.path.join(CASE_OUTPUT_PATH, row["url"].lstrip("/"))
-            print(case_file_name)
-            extract_case_judgement_text(case_file_name)
+            extract_case_judgement_text(case_file_name, skip_existing)
     elif workers > 1:
-        pool_arguments = [(os.path.join(CASE_OUTPUT_PATH, row["url"].lstrip("/")), ) for row_id, row in decision_df.iterrows()]
+        pool_arguments = [(os.path.join(CASE_OUTPUT_PATH, row["url"].lstrip("/")),
+                           skip_existing) for row_id, row in
+                          decision_df.iterrows()]
         with multiprocessing.Pool(workers) as pool:
-            results = pool.starmap(extract_case_judgement_text, pool_arguments)
+            _ = pool.starmap(extract_case_judgement_text, pool_arguments)
     else:
         raise ValueError("workers must be >= 1")
 
 
 if __name__ == "__main__":
     extract_all_cases("data/decisions.csv.gz", workers=int(sys.argv[1]))
-    
-
