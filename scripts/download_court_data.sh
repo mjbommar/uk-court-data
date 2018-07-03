06398bcaa764b42c1dd87a0e1c221d845a5728b1
Michael Bommarito <mike@lexpredict.com>
2018-07-03 13:02:43 -0400

    initial commit with project structure

---
+++ scripts/download_court_data.sh
@@ -0,0 +1,7 @@
+# Clone data
+git clone https://github.com/mjbommar/court-decisions-gb.git
+
+# cd into path and install
+cd court-decisions-gb
+npm install .
+node scripts/scrape.js
---
+++ src/download_case_data.py
@@ -0,0 +1,3 @@
+# Imports
+import pandas
+import requests
